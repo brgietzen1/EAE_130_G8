6d73ed9aa8b65678b2fe7de024a4c7913bfd9b02
William Mangani <wgmangani@ucdavis.edu>
2025-03-03 17:36:22 -0800

    good

--- neutral_point_calc.py
+++ neutral_point_calc.py
@@ -1,24 +1,48 @@
 import numpy as np
 
 #distance from CG back to tail
-l_h = 
+l_h = 17
 #horizontal tail reference area
-S_h = 
+S_h = 162.5
 #mean aerodynamic chord
-chord = 
+chord = 4
 #wing reference area
-S_w = 
+S_w = 162.5
 
 #wing lift coefficient
-#aspect ratio
-AR = 
-
+#wing aspect ratio
+AR_w = 8.5
+#constance from slides
 eta = .97
 #wing sweep
-delta = 
+delta = 15.5
 #mach number
-M =
+M = .16
 
-C_lw = 2*np.pi*AR/(2 + np.sqrt((AR/eta)**2 * (1+np.tan(delta**2 - M**2)) + 4))
+#tail lift coefficient
+#tail aspect ratio
+AR_h = 8.5
+#tail sweep
+delta_h = -15.5
 
-neutral_point = (l_h * S_h / chord / S_w) * C_lh * (C_lw)^(-1) - C_mf * (C_lw)^(-1) 
+#fuselage pitching moment
+#constant depending on wing 1/4 chord position
+K_f = .115
+#fuselage width
+w_f = 3.79
+#fuselage length
+L_f = 35.5
+
+C_l_w = 2*np.pi*AR_w/(2 + np.sqrt((AR_w/eta)**2 * (1+np.tan(delta**2 - M**2)) + 4))
+
+deda = 2/(np.pi*AR_w) * C_l_w
+
+C_l_h_clean = 2*np.pi*AR_h/(2 + np.sqrt((AR_h/eta)**2 * (1+np.tan(delta_h**2 - M**2)) + 4))
+
+C_l_h = C_l_h_clean * (1-deda)
+
+C_m_fus = K_f * w_f**2 * L_f / (S_w * chord) * (C_l_w)**(-1)
+
+neutral_point = (l_h * S_h / chord / S_w) * C_l_h * (C_l_w)**(-1) - C_m_fus * (C_l_w)**(-1)
+
+print(neutral_point)
