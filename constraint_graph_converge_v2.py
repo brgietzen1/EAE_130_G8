8167e9afe4c1126918a6f7365bf095771ce4e498
William Mangani <wgmangani@ucdavis.edu>
2025-03-09 13:27:09 -0700

    ok

--- constraint_graph_converge_v2.py
+++ constraint_graph_converge_v2.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 import Constraint_Graph as cg
 import a4_weight_estimation as we
 
-def takeoff_constraint_converge(s_TO, rho_ground, rho_sea, cl_max_TO, eta_p):
+x = 25.4
+y = 17.8
+
+def takeoff_constraint_converge(x, y, s_TO, rho_ground, rho_sea, cl_max_TO, eta_p):
     S = np.linspace(200, 600, 1000)
     P = np.zeros_like(S)
     for i in range(len(S)):
@@ -12,7 +15,7 @@ def takeoff_constraint_converge(s_TO, rho_ground, rho_sea, cl_max_TO, eta_p):
         tolerance = .001
         converged = False
         while converged == False:
-            results = we.solve_takeoff_weight_2(S_0, P[i], S_design = 12214.12 / 25.4, P_design = 12214.12 / 17.8)
+            results = we.solve_takeoff_weight_2(S_0, P[i], S_design = 12214.12 / x, P_design = 12214.12 / y)
             takeoff_weight, empty_weight_frac, fuel_weight_frac,\
             empty_weight, fuel_weight, iterations = results
             W = takeoff_weight
@@ -26,7 +29,7 @@ def takeoff_constraint_converge(s_TO, rho_ground, rho_sea, cl_max_TO, eta_p):
     return S, P
 
 
-def climb_constraint_converge(e, AR, k_s, cd_0, cl_max_climb, rho, eta_p, G, type):
+def climb_constraint_converge(x, y, e, AR, k_s, cd_0, cl_max_climb, rho, eta_p, G, type):
     S = np.linspace(200, 600, 1000)
     P = np.zeros_like(S)
     for i in range(len(S)):
@@ -35,7 +38,7 @@ def climb_constraint_converge(e, AR, k_s, cd_0, cl_max_climb, rho, eta_p, G, typ
         tolerance = .001
         converged = False
         while converged == False:
-            results = we.solve_takeoff_weight_2(S_0, P[i], S_design = 12214.12 / 25.4, P_design = 12214.12 / 17.8)
+            results = we.solve_takeoff_weight_2(S_0, P[i], S_design = 12214.12 / x, P_design = 12214.12 / y)
             takeoff_weight, empty_weight_frac, fuel_weight_frac,\
             empty_weight, fuel_weight, iterations = results
             W = takeoff_weight
@@ -52,7 +55,7 @@ def climb_constraint_converge(e, AR, k_s, cd_0, cl_max_climb, rho, eta_p, G, typ
     return S, P 
 
 
-def cruise_constraint_converge(e, AR, v_cruise, eta_p, q_cruise, cd_0):
+def cruise_constraint_converge(x, y, e, AR, v_cruise, eta_p, q_cruise, cd_0):
     S = np.linspace(200, 600, 1000)
     P = np.zeros_like(S)
     for i in range(len(S)):
@@ -61,7 +64,7 @@ def cruise_constraint_converge(e, AR, v_cruise, eta_p, q_cruise, cd_0):
         tolerance = .001
         converged = False
         while converged == False:
-            results = we.solve_takeoff_weight_2(S_0, P[i], S_design = 12214.12 / 25.4, P_design = 12214.12 / 17.8)
+            results = we.solve_takeoff_weight_2(S_0, P[i], S_design = 12214.12 / x, P_design = 12214.12 / y)
             takeoff_weight, empty_weight_frac, fuel_weight_frac,\
             empty_weight, fuel_weight, iterations = results
             W = takeoff_weight
@@ -74,7 +77,7 @@ def cruise_constraint_converge(e, AR, v_cruise, eta_p, q_cruise, cd_0):
             P[i] = P_new
     return S, P 
 
-def ceiling_constraint_converge(e, AR, cd_0, rho_cruise, cl_max_cruise, eta_p):
+def ceiling_constraint_converge(x, y, e, AR, cd_0, rho_cruise, cl_max_cruise, eta_p):
     S = np.linspace(200, 600, 1000)
     P = np.zeros_like(S)
     for i in range(len(S)):
@@ -83,7 +86,7 @@ def ceiling_constraint_converge(e, AR, cd_0, rho_cruise, cl_max_cruise, eta_p):
         tolerance = .001
         converged = False
         while converged == False:
-            results = we.solve_takeoff_weight_2(S_0, P[i], S_design = 12214.12 / 25.4, P_design = 12214.12 / 17.8)
+            results = we.solve_takeoff_weight_2(S_0, P[i], S_design = 12214.12 / x, P_design = 12214.12 / y)
             takeoff_weight, empty_weight_frac, fuel_weight_frac,\
             empty_weight, fuel_weight, iterations = results
             W = takeoff_weight
@@ -97,7 +100,7 @@ def ceiling_constraint_converge(e, AR, cd_0, rho_cruise, cl_max_cruise, eta_p):
     return S, P 
 
 
-def maneuver_constraint_converge(e, AR, v_stall_flight, R_turn, q_cruise, cd_0, rho_ground, cl_max, eta_p):
+def maneuver_constraint_converge(x, y, e, AR, v_stall_flight, R_turn, q_cruise, cd_0, rho_ground, cl_max, eta_p):
     S = np.linspace(200, 600, 1000)
     P = np.zeros_like(S)
     for i in range(len(S)):
@@ -106,7 +109,7 @@ def maneuver_constraint_converge(e, AR, v_stall_flight, R_turn, q_cruise, cd_0, 
         tolerance = .001
         converged = False
         while converged == False:
-            results = we.solve_takeoff_weight_2(S_0, P[i], S_design = 12214.12 / 25.4, P_design = 12214.12 / 17.8)
+            results = we.solve_takeoff_weight_2(S_0, P[i], S_design = 12214.12 / x, P_design = 12214.12 / y)
             takeoff_weight, empty_weight_frac, fuel_weight_frac,\
             empty_weight, fuel_weight, iterations = results
             W = takeoff_weight
@@ -119,7 +122,7 @@ def maneuver_constraint_converge(e, AR, v_stall_flight, R_turn, q_cruise, cd_0, 
             P[i] = P_new
     return S, P 
 
-def stall_constraint_converge(v_stall, rho_cruise, cl_max):
+def stall_constraint_converge(x, y, v_stall, rho_cruise, cl_max):
     P = np.linspace(150, 900, 1000)
     S = np.zeros_like(P)
     for i in range(len(P)):
@@ -128,7 +131,7 @@ def stall_constraint_converge(v_stall, rho_cruise, cl_max):
         tolerance = .001
         converged = False
         while converged == False:
-            results = we.solve_takeoff_weight_2(S[i], P_0, S_design = 12214.12 / 25.4, P_design = 12214.12 / 17.8)
+            results = we.solve_takeoff_weight_2(S[i], P_0, S_design = 12214.12 / x, P_design = 12214.12 / y)
             takeoff_weight, empty_weight_frac, fuel_weight_frac,\
             empty_weight, fuel_weight, iterations = results
             W = takeoff_weight
@@ -139,7 +142,7 @@ def stall_constraint_converge(v_stall, rho_cruise, cl_max):
             S[i] = S_new
     return S, P 
 
-def landing_constraint_converge(s_L, s_a, cl_max_land, rho_ground, rho_sea):
+def landing_constraint_converge(x, y, s_L, s_a, cl_max_land, rho_ground, rho_sea):
     P = np.linspace(150, 900, 1000)
     S = np.zeros_like(P)
     for i in range(len(P)):
@@ -148,7 +151,7 @@ def landing_constraint_converge(s_L, s_a, cl_max_land, rho_ground, rho_sea):
         tolerance = .001
         converged = False
         while converged == False:
-            results = we.solve_takeoff_weight_2(S[i], P_0, S_design = 12214.12 / 25.4, P_design = 12214.12 / 17.8)
+            results = we.solve_takeoff_weight_2(S[i], P_0, S_design = 12214.12 / x, P_design = 12214.12 / y)
             takeoff_weight, empty_weight_frac, fuel_weight_frac,\
             empty_weight, fuel_weight, iterations = results
             W = takeoff_weight
@@ -159,7 +162,7 @@ def landing_constraint_converge(s_L, s_a, cl_max_land, rho_ground, rho_sea):
             S[i] = S_new
     return S, P
 
-def stall_constraint_maneuver_converge(rho_cruise, v_stall_flight, cl_max_TO, n):
+def stall_constraint_maneuver_converge(x, y, rho_cruise, v_stall_flight, cl_max_TO, n):
     P = np.linspace(150, 900, 1000)
     S = np.zeros_like(P)
     for i in range(len(P)):
@@ -168,7 +171,7 @@ def stall_constraint_maneuver_converge(rho_cruise, v_stall_flight, cl_max_TO, n)
         tolerance = .001
         converged = False
         while converged == False:
-            results = we.solve_takeoff_weight_2(S[i], P_0, S_design = 12214.12 / 25.4, P_design = 12214.12 / 17.8)
+            results = we.solve_takeoff_weight_2(S[i], P_0, S_design = 12214.12 / x, P_design = 12214.12 / y)
             takeoff_weight, empty_weight_frac, fuel_weight_frac,\
             empty_weight, fuel_weight, iterations = results
             W = takeoff_weight
@@ -179,30 +182,16 @@ def stall_constraint_maneuver_converge(rho_cruise, v_stall_flight, cl_max_TO, n)
             S[i] = S_new
     return S, P 
 
-S_takeoff, P_takeoff, = takeoff_constraint_converge(s_TO=1000, rho_ground=.002378, rho_sea=.002378, cl_max_TO=1.7333, eta_p=0.9)
-S_climb1, P_climb1 = climb_constraint_converge(e=1.03, AR=5, k_s=1.3, cd_0=0.0436, cl_max_climb=1.7333, rho=.002378, eta_p=0.9, G=0.04, type='takeoff')
-S_climb2, P_climb2 = climb_constraint_converge(e=0.97, AR=5, k_s=1.3, cd_0=0.1136, cl_max_climb=2.0, rho=.002378, eta_p=0.9, G=0.03, type='balked')
-S_cruise, P_cruise = cruise_constraint_converge(e=1.10, AR=5, v_cruise=212, eta_p=0.9, q_cruise=(1/2*.002048 * 212**2), cd_0=0.02862)
-S_ceiling, P_ceiling = ceiling_constraint_converge(e=1.10, AR=5, cd_0=0.02862, rho_cruise=.002048, cl_max_cruise=1.6, eta_p=0.9)
-S_maneuver, P_maneuver = maneuver_constraint_converge(e=1.10, AR=5, v_stall_flight=145, R_turn=1800, q_cruise=(1/2*.002048 * 212**2), cd_0=0.02862, rho_ground=.002378, cl_max=1.6, eta_p=0.9)
-S_stall, P_stall = stall_constraint_converge(v_stall=145, rho_cruise=0.002048, cl_max=1.6)
-S_landing, P_landing = landing_constraint_converge(s_L=1500, s_a=500, cl_max_land=2.0, rho_ground=.002378, rho_sea=.002378)
-S_stall_maneuver, P_stall_maneuver = stall_constraint_maneuver_converge(rho_cruise=.002048, v_stall_flight=145, cl_max_TO=1.6, n=(np.sqrt((145**2 / 1800 / 32.2)**2 + 1)))
+S_takeoff, P_takeoff, = takeoff_constraint_converge(x, y, s_TO=1000, rho_ground=.002378, rho_sea=.002378, cl_max_TO=1.7333, eta_p=0.9)
+S_climb1, P_climb1 = climb_constraint_converge(x, y, e=1.03, AR=5, k_s=1.3, cd_0=0.0436, cl_max_climb=1.7333, rho=.002378, eta_p=0.9, G=0.04, type='takeoff')
+S_climb2, P_climb2 = climb_constraint_converge(x, y, e=0.97, AR=5, k_s=1.3, cd_0=0.1136, cl_max_climb=2.0, rho=.002378, eta_p=0.9, G=0.03, type='balked')
+S_cruise, P_cruise = cruise_constraint_converge(x, y, e=1.10, AR=5, v_cruise=212, eta_p=0.9, q_cruise=(1/2*.002048 * 212**2), cd_0=0.02862)
+S_ceiling, P_ceiling = ceiling_constraint_converge(x, y, e=1.10, AR=5, cd_0=0.02862, rho_cruise=.002048, cl_max_cruise=1.6, eta_p=0.9)
+S_maneuver, P_maneuver = maneuver_constraint_converge(x, y, e=1.10, AR=5, v_stall_flight=145, R_turn=1800, q_cruise=(1/2*.002048 * 212**2), cd_0=0.02862, rho_ground=.002378, cl_max=1.6, eta_p=0.9)
+S_stall, P_stall = stall_constraint_converge(x, y, v_stall=145, rho_cruise=0.002048, cl_max=1.6)
+S_landing, P_landing = landing_constraint_converge(x, y, s_L=1500, s_a=500, cl_max_land=2.0, rho_ground=.002378, rho_sea=.002378)
+S_stall_maneuver, P_stall_maneuver = stall_constraint_maneuver_converge(x, y, rho_cruise=.002048, v_stall_flight=145, cl_max_TO=1.6, n=(np.sqrt((145**2 / 1800 / 32.2)**2 + 1)))
 
-
-from scipy.interpolate import interp1d
-
-# Interpolate landing constraint
-sort_idx_land = np.argsort(S_landing)
-S_land_sorted = S_landing[sort_idx_land]
-P_land_sorted = P_landing[sort_idx_land]
-P_land_interp = interp1d(S_land_sorted, P_land_sorted, bounds_error=False, fill_value='extrapolate')
-
-# Interpolate stall constraint if necessary
-sort_idx_stall = np.argsort(S_stall)
-S_stall_sorted = S_stall[sort_idx_stall]
-P_stall_sorted = P_stall[sort_idx_stall]
-P_stall_interp = interp1d(S_stall_sorted, P_stall_sorted, bounds_error=False, fill_value='extrapolate')
 
 plt.figure()
 plt.plot(S_takeoff, P_takeoff/.77, label='Takeoff')
@@ -215,11 +204,11 @@ plt.plot(S_stall, P_stall/.77, label='Stall')
 plt.plot(S_landing, P_landing/.77, label='Landing Distance')
 plt.plot(S_stall_maneuver, P_stall_maneuver/.77, label='Maneuver Stall Distance')
 
-plt.axis([200, 600, 200, 900])
+plt.axis([250, 450, 300, 900])
 x1 = 5
 y1 = 5
 plt.scatter(387.2, 620, color='red', s=70, zorder=2)
-plt.text(346.8-2*x1, 485+3*y1, 'Ceres-100 ($S$ = 350 ft², $P_{shaft}$ = 650 hp, W = 10766 lbs)')
+plt.text(387.2-2*x1, 620+3*y1, 'Ceres-100 ($S$ = 387.2 ft², $P_{shaft}$ = 620 hp, W = 10910 lbs)')
 plt.scatter(306, 680, color='red', s=70, zorder=2)
 plt.text(306+x1, 680-y1, 'AT-402B ($S$ = 306 ft², $P_{shaft}$ = 680 hp, W = 9170 lbs)')
 plt.scatter(365, 750, color='red', s=70, zorder=2)
