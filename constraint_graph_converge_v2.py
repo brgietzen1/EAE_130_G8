12b91f7d16c4d4a2b3a3ec1365b33bfcb7ba3cad
William Mangani <wgmangani@ucdavis.edu>
2025-04-05 09:25:50 -0700

    1

--- constraint_graph_converge_v2.py
+++ constraint_graph_converge_v2.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import Constraint_Graph as cg
 import a4_weight_estimation as we
 
-x = 25.4
-y = 17.8
+x = 21
+y = 16
 
 def takeoff_constraint_converge(x, y, s_TO, rho_ground, rho_sea, cl_max_TO, eta_p):
     S = np.linspace(200, 600, 1000)
@@ -120,7 +120,7 @@ def maneuver_constraint_converge(x, y, e, AR, v_stall_flight, R_turn, q_cruise, 
             if abs(P_new - P[i]) <= tolerance:
                 converged = True
             P[i] = P_new
-    return S, P 
+    return S, P, n 
 
 def stall_constraint_converge(x, y, v_stall, rho_cruise, cl_max):
     P = np.linspace(150, 900, 1000)
@@ -175,7 +175,7 @@ def stall_constraint_maneuver_converge(x, y, rho_cruise, v_stall_flight, cl_max_
             takeoff_weight, empty_weight_frac, fuel_weight_frac,\
             empty_weight, fuel_weight, iterations = results
             W = takeoff_weight
-            wing_loading_landing = cg.stall_constraint_maneuver(rho_cruise, v_stall_flight, cl_max_TO, n, W, W)
+            wing_loading_landing = cg.stall_constraint_maneuver(rho_cruise, v_stall_flight, cl_max_TO, n, W-2000, W)
             S_new = W / wing_loading_landing
             if abs(S_new - S[i]) <= tolerance:
                 converged = True
@@ -185,12 +185,13 @@ def stall_constraint_maneuver_converge(x, y, rho_cruise, v_stall_flight, cl_max_
 S_takeoff, P_takeoff, = takeoff_constraint_converge(x, y, s_TO=1000, rho_ground=.002378, rho_sea=.002378, cl_max_TO=1.7333, eta_p=0.9)
 S_climb1, P_climb1 = climb_constraint_converge(x, y, e=1.03, AR=5, k_s=1.3, cd_0=0.0436, cl_max_climb=1.7333, rho=.002378, eta_p=0.9, G=0.04, type='takeoff')
 S_climb2, P_climb2 = climb_constraint_converge(x, y, e=0.97, AR=5, k_s=1.3, cd_0=0.1136, cl_max_climb=2.0, rho=.002378, eta_p=0.9, G=0.03, type='balked')
-S_cruise, P_cruise = cruise_constraint_converge(x, y, e=1.10, AR=5, v_cruise=212, eta_p=0.9, q_cruise=(1/2*.002048 * 212**2), cd_0=0.02862)
-S_ceiling, P_ceiling = ceiling_constraint_converge(x, y, e=1.10, AR=5, cd_0=0.02862, rho_cruise=.002048, cl_max_cruise=1.6, eta_p=0.9)
-S_maneuver, P_maneuver = maneuver_constraint_converge(x, y, e=1.10, AR=5, v_stall_flight=145, R_turn=1800, q_cruise=(1/2*.002048 * 212**2), cd_0=0.02862, rho_ground=.002378, cl_max=1.6, eta_p=0.9)
-S_stall, P_stall = stall_constraint_converge(x, y, v_stall=145, rho_cruise=0.002048, cl_max=1.6)
+S_cruise, P_cruise = cruise_constraint_converge(x, y, e=1.10, AR=5, v_cruise=212, eta_p=0.9, q_cruise=(1/2*0.001756 * 212**2), cd_0=0.02862)
+S_ceiling, P_ceiling = ceiling_constraint_converge(x, y, e=1.10, AR=5, cd_0=0.02862, rho_cruise=0.001756, cl_max_cruise=1.6, eta_p=0.9)
+S_maneuver, P_maneuver, n = maneuver_constraint_converge(x, y, e=1.10, AR=5, v_stall_flight=169, R_turn=600, q_cruise=(1/2*0.001756 * 212**2), cd_0=0.02862, rho_ground=.002378, cl_max=1.6, eta_p=0.9)
+S_stall, P_stall = stall_constraint_converge(x, y, v_stall=169, rho_cruise=0.001756, cl_max=1.6)
 S_landing, P_landing = landing_constraint_converge(x, y, s_L=1500, s_a=500, cl_max_land=2.0, rho_ground=.002378, rho_sea=.002378)
-S_stall_maneuver, P_stall_maneuver = stall_constraint_maneuver_converge(x, y, rho_cruise=.002048, v_stall_flight=145, cl_max_TO=1.6, n=(np.sqrt((145**2 / 1800 / 32.2)**2 + 1)))
+S_stall_maneuver, P_stall_maneuver = stall_constraint_maneuver_converge(x, y, rho_cruise=0.001756, v_stall_flight=169, cl_max_TO=1.6, n=(np.sqrt((169**2 / 600 / 32.2)**2 + 1)))
+print("The load factor n is:", n)
 
 
 plt.figure()
